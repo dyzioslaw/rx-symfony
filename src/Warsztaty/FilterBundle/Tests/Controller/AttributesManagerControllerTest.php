e557292d13d22179a78a961f806b7c3924364de0
= <=>
2014-10-12 12:32:02 +0200

    fix, + new controller

---
+++ src/Warsztaty/FilterBundle/Tests/Controller/AttributesManagerControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Warsztaty\FilterBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AttributesManagerControllerTest extends WebTestCase
+{
+}
